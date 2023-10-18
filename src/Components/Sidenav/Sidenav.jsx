361a302fd05eaeb7270ea872c7ec4cf1b182c484
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-10-19 00:23:35 +1100

    adding new dropdown function animation

--- src/Components/Sidenav/Sidenav.jsx
+++ src/Components/Sidenav/Sidenav.jsx
@@ -1,40 +1,32 @@
-import './Sidenav.css'
 import React, { useState } from 'react';
+import './Sidenav.css';
 
 function Sidenav() {
+  const [isOpen, setIsOpen] = useState(false);
 
- 
-    const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggleDropdown = () =>{
-        setIsOpen(!isOpen)
-    };
-    
   return (
     <div className='side-nav-container'>
+      <div className='user-icon-container'>
+        <div className='user-icon'></div>
+        <span className='username'><h3>James Wijaya</h3></span>
+      </div>
 
-        <div className='user-icon-container'>
-            <div className='user-icon'></div>
-            <span className='username'><h3>James Wijaya</h3></span>
+      <div className='container-link'>
+        <button className='dropdown-button' onClick={toggleDropdown}><h2>Dashboard</h2></button>
 
+        <div className={`dropdown-content ${isOpen ? 'slideDown' : 'slideUp'}`}>
+          <a href=""><h4>Sales Record</h4></a>
+          <a href=""><h4>Purchase Order</h4></a>
+          <a href=""><h4>Client</h4></a>
+          <a href=""><h4>User Profile</h4></a>
         </div>
- 
-        <div className='container-link'>
-            <button className='dropdown-button' onClick={toggleDropdown}><h2>Dashboard</h2></button>
-
-            {!isOpen && (
-                <div>
-            <a href=""><h3>Sales Record</h3></a>
-            <a href=""><h3>Purchase Order</h3></a>
-            <a href=""><h3>Client</h3></a>
-            <a href=""><h3>User Profle</h3></a>
-            </div>
-           )}
-        </div>
-
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidenav
+export default Sidenav;
