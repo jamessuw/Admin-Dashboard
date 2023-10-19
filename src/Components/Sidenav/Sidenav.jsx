cf97a88b3bcba0f18d495f927b6fa51651b9039d
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-10-19 16:18:36 +1100

    developing UI sideNav

--- src/Components/Sidenav/Sidenav.jsx
+++ src/Components/Sidenav/Sidenav.jsx
@@ -1,32 +1,44 @@
-import React, { useState } from 'react';
 import './Sidenav.css';
+import React, { useState } from 'react';
 
 function Sidenav() {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <div className='side-nav-container'>
-      <div className='user-icon-container'>
-        <div className='user-icon'></div>
-        <span className='username'><h3>James Wijaya</h3></span>
-      </div>
+    return (
+        <div className='side-nav-container'>
+            <div className='user-icon-container'>
+                <div className='user-icon'></div>
+                <span className='username'>
+                    <h3>James Wijaya</h3>
+                </span>
+            </div>
 
-      <div className='container-link'>
-        <button className='dropdown-button' onClick={toggleDropdown}><h2>Dashboard</h2></button>
+            <div className='container-link'>
+                <button className='dropdown-button' onClick={toggleDropdown}>
+                    <h2>Dashboard</h2>
+                </button>
 
-        <div className={`dropdown-content ${isOpen ? 'slideDown' : 'slideUp'}`}>
-          <a href=""><h4>Sales Record</h4></a>
-          <a href=""><h4>Purchase Order</h4></a>
-          <a href=""><h4>Client</h4></a>
-          <a href=""><h4>User Profile</h4></a>
+                <div className={`dropdown-content ${isOpen ? 'show' : ''}`}>
+                    <a href="">
+                        <h4>Sales Record</h4>
+                    </a>
+                    <a href="">
+                        <h4>Purchase Order</h4>
+                    </a>
+                    <a href="">
+                        <h4>Client</h4>
+                    </a>
+                    <a href="">
+                        <h4>User Profle</h4>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Sidenav;
