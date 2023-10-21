9bc2b317c6349258ea1c1581d565dc16afcaec90
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-10-21 15:16:15 +1100

    adding header in the create form page

--- src/Components/CreateForm/CreateForm.jsx
+++ src/Components/CreateForm/CreateForm.jsx
@@ -1,6 +1,7 @@
 import Sidenav from '../Sidenav/Sidenav'
 import './createForm.css'
 import React, { useState } from 'react';
+import HeaderDash from '../Header/HeaderDash';
 
 
 
@@ -19,6 +20,7 @@ function CreateForm() {
 
   return (
  <section className='form-component'>
+<HeaderDash/>
     <div className='form-container'>
   <Sidenav/>
     </div>
