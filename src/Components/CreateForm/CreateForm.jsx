f626cad05fd3b245dfeb535e22d68e006dcf2c17
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-10-23 23:10:40 +1100

    start developing grid list in create form

--- src/Components/CreateForm/CreateForm.jsx
+++ src/Components/CreateForm/CreateForm.jsx
@@ -8,15 +8,7 @@ import HeaderDash from '../Header/HeaderDash';
 
 function CreateForm() {
 
-    const [poNumber, setPoNumber] = useState('');
-    const [vendorName, setVendorName] = useState('');
-    const [quantity, setQuantity] = useState(1);
   
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Here you can handle the form data, for example, send it to an API or process it further.
-      console.log('Purchase Order Submitted:', { poNumber, vendorName, quantity });
-    };
 
   return (
  <section className='form-component'>
@@ -27,27 +19,10 @@ function CreateForm() {
    
    <div className='form-section-ctr'>
    
-   <div className='form-section'>
- <h1>Purchase Order</h1>
-   <form onSubmit={handleSubmit}>
-      <label>
-        PO Number:
-        <input type="text" value={poNumber} onChange={(e) => setPoNumber(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Vendor Name:
-        <input type="text" value={vendorName} onChange={(e) => setVendorName(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Quantity:
-        <input type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
-   </div>
+   
+
+   
+  
    </div>
 
     </section>
