29203381644d2f67ecce9f44a09d911d25ce78c1
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-10-26 17:13:16 +1100

    adding function passing data from POForm to createForm

--- src/Components/CreateForm/CreateForm.jsx
+++ src/Components/CreateForm/CreateForm.jsx
@@ -4,71 +4,80 @@ import React, { useState } from 'react';
 import HeaderDash from '../Header/HeaderDash';
 import PurchaseOrderForm from './PurchaseOrderForm';
 
-
-
-
 function CreateForm() {
 
-    const [showPopUp, setShowPopUp] = useState(false);
+const [showPopUp, setShowPopUp] = useState(false);
 
-    const togglePopUp = () => {
-      setShowPopUp(!showPopUp);
-    };
+const togglePopUp = () => {
+setShowPopUp(!showPopUp);
+};
 
-  return (
- <section className='form-component'>
-<HeaderDash/>
+const [formData, setFormData] = useState([]);
+const [itemData, setItemData] = useState({});
+
+const handleItemInputChange = (item) => {
+setItemData(item); // Update the item data when input fields change
+};
+
+const handleFormSubmit = () => {
+setFormData([...formData, itemData]); // Add the item data to the form data
+};
+
+return (
+<section className='form-component'>
+    <HeaderDash />
     <div className='form-container'>
-  <Sidenav/>
-    </div>
-   
-
-   <div className='container-2'>
-
-   <div className='create-form'>
-   <button onClick={togglePopUp}>Create Form</button>
+        <Sidenav />
     </div>
 
+    <div className='container-2'>
 
-   <div className='table-container'>
-    <table>
-        <tr>
-            <th>No</th>
-            <th>Description</th>
-            <th>Quantity</th>
-            <th>unit</th>
-            <th>Unit price</th>
-            <th>total price</th>
-        </tr>
-        <tr>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-            <td>test</td>
-        </tr>
-    </table>
-   </div>
-   
+        <div className='create-form'>
+            <button onClick={togglePopUp}>Create Form</button>
+        </div>
 
-   
-  
-   </div>
-   <div className='pop-up-create-form'>
-   {showPopUp && (
-          <div className='modal-overlay'>
+        <div className='table-container'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Unit</th>
+                        <th>Cost per Unit</th>
+                        <th>Total Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {formData.map((data, index) => (
+                    <tr key={index}>
+                        <td>{data.no}</td>
+                        <td>{data.description}</td>
+                        <td>{data.quantity}</td>
+                        <td>{data.unit}</td>
+                        <td>{data.costPerUnit}</td>
+                        <td>{data.totalPrice}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <div className='pop-up-create-form'>
+        {showPopUp && (
+        <div className='modal-overlay'>
             <div className='modal'>
-              <span className='close' onClick={togglePopUp}>&times;</span>
-            <PurchaseOrderForm/>
-              {/* Additional form elements can be added here */}
+                <span className='close' onClick={togglePopUp}>&times;</span>
+                <PurchaseOrderForm onInputChange={handleItemInputChange} onSubmit={handleFormSubmit} />
+                {/* Additional form elements can be added here */}
             </div>
-          </div>
-)}
-     </div>
+        </div>
+        )}
+    </div>
 
-    </section>
-  )
+</section>
+)
 }
 
 export default CreateForm
--- src/Components/CreateForm/PurchaseOrderForm.jsx
+++ src/Components/CreateForm/PurchaseOrderForm.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import { useState } from "react";
 
-function PurchaseOrderForm() {
+function PurchaseOrderForm({ onInputChange, onSubmit }) {
+const [item, setItem] = useState({
+no: "",
+description: "",
+quantity: "",
+unit: "",
+costPerUnit: "",
+totalPrice: ""
+});
+
+const handleItemInputChange = (e) => {
+const { name, value } = e.target;
+setItem({ ...item, [name]: parseInt(value, 10) || "" });
+onInputChange({ ...item, [name]: parseInt(value, 10) || "" });
+};
+
+const handleSubmit = (e) => {
+e.preventDefault();
+onSubmit(); // Notify the parent component that the form is submitted
+};
+
 return (
 <div>
     <div className="PO-container">
         <h1>Purchase Order</h1>
         <h2>General Information</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>To</label>
             <input></input>
 
@@ -23,40 +43,46 @@ return (
                 <h2>Items Informations</h2>
                 <div className="items-container-popup">
                     <div className="container-field-1">
-                    <div className="label-input">
-                        <label>No</label>
-                        <input></input>
-                    </div>
-                    <div className="label-input">
-                        <label>Descriptions</label>
-                        <input></input>
+                        <div className="label-input">
+                            <label>No</label>
+                            <input type="number" name="no" value={item.no} onChange={handleItemInputChange}
+                                placeholder="No"></input>
+                        </div>
+                        <div className="label-input">
+                            <label>Descriptions</label>
+                            <input></input>
                         </div>
 
-                    <div className="label-input">
-                        <label>Quantity Items</label>
-                        <input></input>
-                    </div></div>
+                        <div className="label-input">
+                            <label>Quantity Items</label>
+                            <input></input>
+                        </div>
+                    </div>
 
                     <div className="conatiner-field-2">
-                    <div className="label-input">
-                        <label>Unit</label>
-                        <input></input></div>
-               
-                <div className="label-input">
-                    <label>Cost per Unit</label>
-                    <input></input></div>
+                        <div className="label-input">
+                            <label>Unit</label>
+                            <input></input></div>
 
-                <div className="label-input">
+                        <div className="label-input">
+                            <label>Cost per Unit</label>
+                            <input></input></div>
 
-                    <label>Quantity Item</label>
-                    <input></input>
+                        <div className="label-input">
+
+                            <label>Quantity Item</label>
+                            <input type="number" name="quantity" value={item.quantity} onChange={handleItemInputChange}
+                                placeholder="quantity"></input>
+                        </div>
+                        <div className="label-input">
+                            <label>Total price</label>
+                            <input></input>
+                        </div>
+                    </div>
                 </div>
-                <div className="label-input">
-                    <label>Total price</label>
-                    <input></input>
-                </div></div> </div>
             </div>
-<div className="submit-conatiner"><button className="PO-add"><h3>add purchase order</h3></button></div>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+
         </form>
     </div>
 </div>
