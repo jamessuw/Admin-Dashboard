ffb075d3dbd328e0c430dea6c5a979a0df42c234
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-11-13 23:35:33 +1100

    rework server.js

--- Server/server.js
+++ Server/server.js
@@ -1,60 +1,15 @@
-// Import dotenv and load environment variables
-require('dotenv').config();
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');  // Import cors module
-const mongoose = require('mongoose');
-
+const express = require("express");
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: 'http://localhost:5000', // Update with your React app's URL
-    optionsSuccessStatus: 200,
-  };
-  
-  app.use(cors(corsOptions));
-  app.use(bodyParser.json());
-  
-// Use environment variable for MongoDB URI
-const MONGODB_URI = process.env.MONGODB_URI;
-
-async function connect() {
-  try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-}
-
-connect();
-
-const purchaseOrderSchema = new mongoose.Schema({
-  no: { type: Number, required: true },
-  description: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  unit: { type: String, required: true },
-  costPerUnit: { type: Number, required: true },
-  date: { type: Date, required: true },
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://oktober30xZ:<password>@cluster0.e67fhar.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const PurchaseOrder = mongoose.model('PurchaseOrder', purchaseOrderSchema);
-
-app.post('/api/save-purchase-order', async (req, res) => {
-  try {
-    const purchaseOrder = new PurchaseOrder(req.body);
-    await purchaseOrder.save();
-    res.status(200).json({ message: 'Purchase order data saved successfully!' });
-  } catch (error) {
-    console.error('Error occurred while saving purchase order data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+app.get('/', (req, res) => {
+  res.send('Hello, this is your root route!');
 });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
+const { connect } = require('./Server/server');
+
+const port = process.env.PORT || 5000;
+
+connect().then((app) => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
